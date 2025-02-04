4cd92196fa79e27934760e2c80be9b281fe3a2ab
Farouk-Abolaban <faroukabolaban@gmail.com>
2025-02-04 01:04:42 -0500

    Some corrections for deployment

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
-const inter = Inter({subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Cashie",
@@ -13,21 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        {/* header */}
-        <Header/>
-        
-        <main className="min-h-screen">{children}</main>
-        <Toaster richColors />
-        {/* footer */}
-        <footer className = "bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made by Farouk Abolaban</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          {/* header */}
+          <Header />
+
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made by Farouk Abolaban</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
