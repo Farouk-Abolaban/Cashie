b5c528fb0cc0a7b6eb4222cb2f3b5bc3aea0b9dd
Farouk-Abolaban <faroukabolaban@gmail.com>
2025-02-04 00:36:27 -0500

    Fixed dashboard overview charts

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -5,96 +5,119 @@ import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 const serializeTransaction = (obj) => {
-    const serialized = { ...obj };
+  const serialized = { ...obj };
 
-    if (obj.balance) {
-        serialized.balance = obj.balance.toNumber();
-    }
+  serialized.balance =
+    obj.balance instanceof Object && "toNumber" in obj.balance
+      ? obj.balance.toNumber()
+      : Number(obj.balance) || 0;
 
-    if (obj.amount) {
-        serialized.balance = obj.balance.toNumber();
-    }
+  serialized.amount =
+    obj.amount instanceof Object && "toNumber" in obj.amount
+      ? obj.amount.toNumber()
+      : Number(obj.amount) || 0;
 
-    return serialized;
+  return serialized;
 };
 
 export async function createAccount(data) {
-    try{
-        const {userId} = await auth();
-        if (!userId) throw new Error("Unauthorized");
-
-        const user = await db.user.findUnique({
-            where: { clerkUserId: userId },
-        });
-
-        if (!user) {
-            throw new Error("User not found");
-        }
-
-        //Convert balance to float before saving
-        const balanceFloat = parseFloat(data.balance)
-        if(isNaN(balanceFloat)){
-            throw new Error("Invalid balance amount");
-        }
-
-        //Check if this is the user's first account
-        const existingAccounts = await db.account.findMany({
-            where: { userId: user.id},
-        });
-
-        const shouldBeDefault = existingAccounts.length == 0 ? true : data.isDefault;
-
-        if (shouldBeDefault) {
-            await db.account.updateMany({
-                where: { userId: user.id, isDefault: true},
-                data: { isDefault: false },
-            });
-        }
-
-        const account = await db.account.create({
-            data: {
-                ...data, 
-                balance: balanceFloat, 
-                userId: user.id, 
-                isDefault: shouldBeDefault,
-            },
-        });
-
-        const serializedAccount = serializeTransaction(account);
-
-        revalidatePath("/dashboard");
-        return { success: true, data: serializedAccount };
-    } catch (error) {
-        throw new Error(error.message);
-    }
-
-}   
-
-export async function getUserAccounts() {
-    const {userId} = await auth();
+  try {
+    const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
 
     const user = await db.user.findUnique({
-        where: { clerkUserId: userId },
+      where: { clerkUserId: userId },
     });
 
     if (!user) {
-        throw new Error("User not found");
+      throw new Error("User not found");
     }
 
-    const accounts = await db.account.findMany({
-        where: {userId: user.id},
-        orderBy: { createdAt: "desc" },
-        include: {
-            _count: {
-                select: {
-                    transactions: true,
-                },
-            },
-        },
+    //Convert balance to float before saving
+    const balanceFloat = parseFloat(data.balance);
+    if (isNaN(balanceFloat)) {
+      throw new Error("Invalid balance amount");
+    }
+
+    //Check if this is the user's first account
+    const existingAccounts = await db.account.findMany({
+      where: { userId: user.id },
     });
 
-    const serializedAccount = accounts.map(serializeTransaction);
+    const shouldBeDefault =
+      existingAccounts.length == 0 ? true : data.isDefault;
 
-    return serializedAccount;
+    if (shouldBeDefault) {
+      await db.account.updateMany({
+        where: { userId: user.id, isDefault: true },
+        data: { isDefault: false },
+      });
+    }
+
+    const account = await db.account.create({
+      data: {
+        ...data,
+        balance: balanceFloat,
+        userId: user.id,
+        isDefault: shouldBeDefault,
+      },
+    });
+
+    const serializedAccount = serializeTransaction(account);
+
+    revalidatePath("/dashboard");
+    return { success: true, data: serializedAccount };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getUserAccounts() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const accounts = await db.account.findMany({
+    where: { userId: user.id },
+    orderBy: { createdAt: "desc" },
+    include: {
+      _count: {
+        select: {
+          transactions: true,
+        },
+      },
+    },
+  });
+
+  const serializedAccount = accounts.map(serializeTransaction);
+
+  return serializedAccount;
+}
+
+export async function getDashboardData() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  // Get all user transactions
+  const transactions = await db.transaction.findMany({
+    where: { userId: user.id },
+    orderBy: { date: "desc" },
+  });
+
+  return transactions.map(serializeTransaction);
 }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,16 +1,19 @@
-import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import React from "react";
+import React, { Suspense } from "react";
 import AccountCard from "./_components/account-card";
 import { getCurrentBudget } from "@/actions/budget";
 import BudgetProgress from "./_components/budget-progress";
+import { DashboardOverview } from "./_components/transaction-overview";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
 
   const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  const transactions = await getDashboardData();
 
   let budgetData = null;
   if (defaultAccount) {
@@ -28,6 +31,12 @@ async function DashboardPage() {
       )}
 
       {/* Overview */}
+      <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview
+          accounts={accounts}
+          transactions={transactions || []}
+        />
+      </Suspense>
 
       {/* Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
